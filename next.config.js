6a9c8b09a4159bf209cff1a8a29428299cd551c1
Eric Kang <kyungwoo@hanyang.ac.kr>
2025-12-28 15:03:15 +0900

    Fix next.config for ES modules

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone', // For Netlify optimization
-  images: {
-    unoptimized: true, // If you're having image issues on Netlify
-  },
-}
-
-module.exports = nextConfig
